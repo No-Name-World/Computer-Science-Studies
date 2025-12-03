01e4dd7399c8b44d9b3b55b919aefc247479e69e
HUDSOn C. <159186112+No-Name-World@users.noreply.github.com>
2025-12-04 01:40:08 +0800

    ok

--- Concurrent Programming/Exercise/Exer0008_ThreadState/ShortRunnableProcess.java
+++
@@ -1,9 +0,0 @@
-package Exer0008_Threadstate;
-public class ShortRunnableProcess implements Runnable{
-    
-    public void run()
-    {
-        System.out.println(Thread.currentThread().getName() +
-                ": Short Runnable Process been Fully Executed.");
-    }
-}
